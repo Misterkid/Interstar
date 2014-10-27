0bd80973d7f61860f55da9a7fe19da1e3b0da6a2
Mark de Heij <markdeheij@hotmail.com>
2014-10-27 15:21:50 +0100

    Spawning them Tomatoes MdH
    Trying to get a wavespawn script

---
+++ Defend And Blend/Assets/Scripts/WaveSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSystem : MonoBehaviour 
+{
+    public GameObject Tomato;
+    public Vector3 spawnValues;
+	
+    // Use this for initialization
+	void Start () 
+    {
+        spawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        spawnWaves();
+	}
+
+    void spawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity; 
+        Instantiate(Tomato, spawnPosition, spawnRotation);
+    }
+}
