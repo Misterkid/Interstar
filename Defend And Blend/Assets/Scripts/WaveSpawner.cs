70cadd7f8c60bb4026c481e5316d1844f583682a
mrmaarek <markdeheij@hotmail.com>
2014-10-27 19:31:47 +0100

    Made an simple wavesystem.
    Copied Eddy's scène so i had the same startpoints. Now there are
    spawning 3 strawberrys every wave., after the wave we are waiting 20
    seconds for the new wave is coming. this wil again be just strawberrys.
    
    Didn't tried to deploy an array of enemies,.

---
+++ Defend And Blend/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public bool isSpawning;
+    public GameObject[] enemies;
+    public Vector3 spawnValues;
+    public GameObject StrawBerry;
+
+    //How many enemies should have one wave?
+    public int waveCount = 3;
+    public int currentWave = 1;
+
+    //Wait time after a wave is finished
+    public float waitBetweenWaves = 15;
+    
+    //Seconds between the spawn of next enemy in wave.
+    public float timeBetweenNextEnemy = 1;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("Starting with Wave:" + currentWave);
+        StartCoroutine(SpawnWaves());
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(0);
+        Debug.Log("################################");
+        
+        while (true)
+        {
+            Debug.Log("Wave #" + currentWave + "started");
+            //Check if the max number of enemies is already spawned
+            for (int i = 0; i < waveCount; i++)
+            {
+                
+                Debug.Log("################################");
+                
+                Vector3 spawnPosition = new Vector3(Random.Range(10, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(StrawBerry, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(timeBetweenNextEnemy);
+                Debug.Log("Waiting #" + timeBetweenNextEnemy + "sec to deploy next fruit");
+            }
+            Debug.Log("Waiting #" + waitBetweenWaves + "seconds for the next wave");
+            yield return new WaitForSeconds(waitBetweenWaves);
+            Debug.Log("Waiting finished, preparing next wave!");
+            Debug.Log("################################");
+            currentWave += 1;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
