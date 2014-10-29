71efff8db3ca0f217dd505e42f137b5286d25d93
Eduard Meivogel <eddymeivogel@gmail.com>
2014-10-29 13:54:31 +0100

    Added helping hand
    Added helping hand

---
+++ Defend And Blend/Assets/Scripts/HelpingHand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpingHand : MonoBehaviour 
+{
+    public GameObject rightObject;
+    public GameObject leftObject;
+    private float rightObjectStart;
+    private float leftObjectStart;
+	// Use this for initialization
+	void Start () 
+    {
+        rightObjectStart = rightObject.transform.localPosition.x;
+        leftObjectStart = leftObject.transform.localPosition.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    //Controls
+        transform.Translate((Input.GetAxis("Horizontal") * 10) * Time.deltaTime, (Input.GetAxis("Vertical") * 10) * Time.deltaTime, 0);
+        //pressure
+        if(rightObject != null && leftObject != null)
+        {
+            float squeezePressure = rightObjectStart - (rightObjectStart * Input.GetAxis("RTrigger"));//Get the trigger / pressure value.
+            rightObject.transform.localPosition = new Vector3(squeezePressure, rightObject.transform.localPosition.y, rightObject.transform.localPosition.z);
+            leftObject.transform.localPosition = new Vector3(-squeezePressure, leftObject.transform.localPosition.y, leftObject.transform.localPosition.z);
+           // collider.bounds.size = new Vector3(collider.bounds.size.x, collider.bounds.size.y, collider.bounds.size.z);
+            BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>() as BoxCollider;
+            boxCollider.size = new Vector3( (rightObject.transform.localPosition.x) * 2, boxCollider.size.y, boxCollider.size.z);
+        }
+	}
+    private void OnTriggerEnter(Collider collider)
+    {
+        //Debug.Log(collider.name);
+    }
+}
