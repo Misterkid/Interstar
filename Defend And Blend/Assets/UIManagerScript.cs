f10c7a3d4a6fc0a65f627f4b5d82e5401f3c06cc
Mark de Heij <markdeheij@hotmail.com>
2014-11-03 13:17:29 +0100

    First 'Options' Animation
    Just trying to get an animation work based on clicks in the menu.

--- Defend And Blend/Assets/UIManagerScript.cs
+++ Defend And Blend/Assets/UIManagerScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UIManagerScript : MonoBehaviour {
+public class UIManagerScript : MonoBehaviour 
+{
+    public Animator OptionsTextAnimator;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        OptionsTextAnimator.SetBool("isHidden", true);
 	}
 	
 	public void StartGame()
     {
         Application.LoadLevel("Eddy_test");
+    }
+
+    public void openOptions()
+    {
+        //Wanneer isHidden al true is, maak hem false
+        if (OptionsTextAnimator.GetBool("isHidden"))
+        {
+            OptionsTextAnimator.SetBool("isHidden", false);
+        }
+        else if (!OptionsTextAnimator.GetBool("isHidden"))
+        {
+            OptionsTextAnimator.SetBool("isHidden", true);
+        }
     }
 }
