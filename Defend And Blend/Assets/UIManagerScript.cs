4e54d812ab2c56ce5a7f4a572f0d936428ba64a9
Mark de Heij <markdeheij@hotmail.com>
2014-11-03 14:22:59 +0100

    Added Highscores Animation
    this is epic awesomeness, just playing with some animation in the main
    menu! :') check it out now!

--- Defend And Blend/Assets/UIManagerScript.cs
+++ Defend And Blend/Assets/UIManagerScript.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class UIManagerScript : MonoBehaviour 
 {
     public Animator OptionsTextAnimator;
+    public Animator HighscoresAnimator;
 
 	// Use this for initialization
 	void Start () 
     {
         OptionsTextAnimator.SetBool("isHidden", true);
+        HighscoresAnimator.SetBool("isHidden", true);
 	}
 	
 	public void StartGame()
@@ -21,11 +23,26 @@ public class UIManagerScript : MonoBehaviour
         //Wanneer isHidden al true is, maak hem false
         if (OptionsTextAnimator.GetBool("isHidden"))
         {
+            HighscoresAnimator.SetBool("isHidden", true);
             OptionsTextAnimator.SetBool("isHidden", false);
         }
         else if (!OptionsTextAnimator.GetBool("isHidden"))
         {
             OptionsTextAnimator.SetBool("isHidden", true);
+        }
+    }
+
+    public void openHighScores()
+    {
+        //Wanneer isHidden al true is, maak hem false
+        if (HighscoresAnimator.GetBool("isHidden"))
+        {
+            HighscoresAnimator.SetBool("isHidden", false);
+            OptionsTextAnimator.SetBool("isHidden", true);
+        }
+        else if (!HighscoresAnimator.GetBool("isHidden"))
+        {
+            HighscoresAnimator.SetBool("isHidden", true);
         }
     }
 }
