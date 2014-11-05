742123791368ca5a00aa13e1d04edee512e94ef6
Eduard Meivogel <eddymeivogel@gmail.com>
2014-11-05 10:35:11 +0100

    Added Game initializor :crossed_flags:
    Added Game initializor :crossed_flags:

---
+++ Defend And Blend/Assets/Scripts/GameInitializor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInitializor : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	    //Init game stuff here
+        SoundManager.Load();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
