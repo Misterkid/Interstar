43f162aae47e12884bcdf99cb1f0b9732244e9d1
Mark de Heij <markdeheij@hotmail.com>
2014-11-13 16:16:13 +0100

    Trying to debug the healthbar
    Added a few comments, and debugs.
    Still working on it though yo!

--- Defend And Blend/Assets/Scripts/Defendable.cs
+++ Defend And Blend/Assets/Scripts/Defendable.cs
@@ -16,6 +16,7 @@ public class Defendable : MonoBehaviour
     private float cachedY;
     private float minXValue;
     private float maxXValue;
+    private float currentXValue;
 
     public int maxHealth = 100;
     private int currentHealth;
@@ -35,59 +36,71 @@ public class Defendable : MonoBehaviour
     public Text healthText;
     public Image visualHealth;
 
-    public float cooldown;
-    private bool onCooldown;
     //End of Testing some hpbars.
 
 	// Use this for initialization
 	void Start () 
     {
+        //Caches the healthbars' startposition. (5.926359)
         cachedY = healthTransform.position.y;
+        //Caches the the max value of the xPos is the start position (12)			
         maxXValue = healthTransform.position.x;
+        //The minValue of the xPos is startPos - the width of the bar
+        // minXvalue = 12 - 250 = -238.			
         minXValue = healthTransform.position.x - healthTransform.rect.width;
+
+        //In the start, the currentHealth is always the maxHealth (100)
         currentHealth = maxHealth;
 
-        onCooldown = false;
+        Debug.Log("("+currentHealth +"x" + 0 +") x (" + maxXValue +"-" + minXValue + ") / (" +maxHealth +"-" +0 +") "+minXValue);
 	}
 	
     private void HandleHealth()
     {
         healthText.text = "Health: " + currentHealth;
 
-        float currentXValue = MapValues(currentHealth, 0, maxHealth, minXValue, maxXValue);
+        currentXValue = Map(currentHealth, 0, maxHealth, minXValue, maxXValue);
+
+        //private float MapValues(float x, float in_min, float in_max, float out_min, float out_max)
+        Debug.Log("("+currentHealth +"x" + 0 +") x (" + maxXValue +"-" + minXValue + ") / (" +maxHealth +"-" +0 +") "+minXValue);
+        
+
+        Debug.Log(Map(currentHealth, 0, maxHealth, minXValue, maxXValue));
 
         healthTransform.position = new Vector3(currentXValue, cachedY);
 
         if (currentHealth > maxHealth / 2) // I have more than 50% health.
         {
-            visualHealth.color = new Color32((byte)MapValues(currentHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
+            visualHealth.color = new Color32((byte)Map(currentHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
         }
         else // I have less than 50% health. 
         {
-            visualHealth.color = new Color32(255, (byte)MapValues(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
+            visualHealth.color = new Color32(255, (byte)Map(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
         }
     }
 
-    IEnumerator CooldownDMG()
-    {
-        onCooldown = true;
-        yield return new WaitForSeconds(cooldown);
-        onCooldown = false;
-    }
-
+    
 
     public void DoDamage(float pain)
     {
-        if (!onCooldown && currentHealth > 0)
+        if (currentHealth > 0)
         {
-            StartCoroutine(CooldownDMG());
+            
             health -= pain;
-            CurrentHealth -= 1;
+            CurrentHealth -= 2;
         }
     }
-
-    private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+    /// <summary>
+    /// This method maps a range of number into another range
+    /// </summary>
+    /// <param name="x">The value to evaluate</param>
+    /// <param name="in_min">The minimum value of the evaluated variable</param>
+    /// <param name="in_max">The maximum value of the evaluated variable</param>
+    /// <param name="out_min">The minum number we want to map to</param>
+    /// <param name="out_max">The maximum number we want to map to</param>
+    /// <returns></returns>
+    public float Map(float x, float in_min, float in_max, float out_min, float out_max)
     {
-        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 }
