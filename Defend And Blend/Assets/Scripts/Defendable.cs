8f87c9ac4e30f1d68f492bda0ecb76d2a552f034
Mark de Heij <markdeheij@hotmail.com>
2014-11-17 14:17:58 +0100

    Working HealthBar implemented
    Fixxed some issues that make the HealthBar work. Now it's working like a
    charm. Still need to clean up Defendable.cs though.

--- Defend And Blend/Assets/Scripts/Defendable.cs
+++ Defend And Blend/Assets/Scripts/Defendable.cs
@@ -2,15 +2,8 @@
 using System.Collections;
 using UnityEngine.UI;
 
-//IMPLEMENTED HEALTHBAR TUTORIAL:
-// REFERENCE BELOW!
-// http://www.youtube.com/watch?v=NgftVg3idB4
-
-
 public class Defendable : MonoBehaviour 
-{
-    public float health = 100;
-    
+{   
     //Testing some shizzle
     /// <summary>
     /// The players current health
@@ -20,7 +13,7 @@ public class Defendable : MonoBehaviour
     /// <summary>
     /// The player's max health
     /// </summary>
-    public int maxHealth;
+    public float maxHealth;
 
     /// <summary>
     /// The healt's transform, this is used for moving the object
@@ -37,20 +30,16 @@ public class Defendable : MonoBehaviour
     /// </summary>
     public Image visualHealth;
 
-    /// <summary>
-    /// The health's y pos
-    /// </summary>
-    private float cachedY;
-
+ 
     /// <summary>
     /// The minimum value of the health's x pos
     /// </summary>
-    private float minXValue;
+    private float zeroHealthXPos;
 
     /// <summary>
     /// The max value of the health's x pos
     /// </summary>
-    private float maxXValue;
+    private float fullHealthXPos;
 
     /// <summary>
     /// The current xValue of the health
@@ -72,55 +61,49 @@ public class Defendable : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-        //Caches the healthbars' startposition. (5.926359)
-        cachedY = healthTransform.position.y;
+        Debug.Log(currentHealth);
+
+        //Debug.Log(healthTransform.localPosition);
 
         //Caches the the max value of the xPos is the start position (12)			
-        maxXValue = healthTransform.position.x;
+        fullHealthXPos = healthTransform.localPosition.x;
+
+        Debug.Log(healthTransform.rect.width);
+        
         //The minValue of the xPos is startPos - the width of the bar
         // minXvalue = 12 - 250 = -238.			
-        minXValue = healthTransform.position.x - healthTransform.rect.width;
+        zeroHealthXPos = healthTransform.localPosition.x - healthTransform.rect.width;
 
         //In the start, the currentHealth is always the maxHealth (100)
         currentHealth = maxHealth;
 
-        Debug.Log("("+currentHealth +"x" + 0 +") x (" + maxXValue +"-" + minXValue + ") / (" +maxHealth +"-" +0 +") "+minXValue);
 	}
 	
     private void HandleHealth()
     {
         healthText.text = "Health: " + currentHealth;
-
-        currentXValue = Map(currentHealth, 0, maxHealth, minXValue, maxXValue);
-
-        //private float MapValues(float x, float in_min, float in_max, float out_min, float out_max)
-        Debug.Log("("+currentHealth +"x" + 0 +") x (" + maxXValue +"-" + minXValue + ") / (" +maxHealth +"-" +0 +") "+minXValue);
-        Debug.Log (currentXValue);
-
-        Debug.Log(Map(currentHealth, 0, maxHealth, minXValue, maxXValue));
-
-        healthTransform.position = new Vector3(currentXValue, cachedY);
+        currentXValue = handleHealthColor(currentHealth, 0, 100, zeroHealthXPos, fullHealthXPos);
+       
+        float percentage = maxHealth / 100;// 1%
+        float current = currentHealth / percentage; //175 / 1 = 1.75
+                                                    //-(100 - 1.75) =
+                                                    //
+        healthTransform.localPosition = new Vector3(-(100 - current), 0, 0);
 
         if (currentHealth > maxHealth / 2) // I have more than 50% health.
         {
-            visualHealth.color = new Color32((byte)Map(currentHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
+            visualHealth.color = new Color32((byte)handleHealthColor(currentHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
         }
         else // I have less than 50% health. 
         {
-            visualHealth.color = new Color32(255, (byte)Map(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
+            visualHealth.color = new Color32(255, (byte)handleHealthColor(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
         }
     }
-
-    
 
     public void DoDamage(float pain)
     {
         if (currentHealth > 0)
         {
-            
-            health -= pain;
-            //CurrentHealth -= 2;
-
             CurrentHealth -= pain;
         }
     }
@@ -133,8 +116,11 @@ public class Defendable : MonoBehaviour
     /// <param name="out_min">The minum number we want to map to</param>
     /// <param name="out_max">The maximum number we want to map to</param>
     /// <returns></returns>
-    public float Map(float x, float in_min, float in_max, float out_min, float out_max)
+    public float handleHealthColor(float x, float in_min, float in_max, float out_min, float out_max)
     {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
+
+
+    
 }
