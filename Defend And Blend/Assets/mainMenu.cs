8ca78217279a906fd6298b31d5eb824609c78a97
Mark de Heij <markdeheij@hotmail.com>
2014-11-19 16:46:11 +0100

    Startmenu scene updated
    Added the first animation, still working on it.

---
+++ Defend And Blend/Assets/mainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour 
+{
+    public Animator RP_StartGame;
+	// Use this for initialization
+	void Awake() 
+	{
+        Debug.Log("Awake Function has been called.");
+        RP_StartGame.SetBool("isVisible", false);
+        Debug.Log(RP_StartGame.GetBool("isVisible")); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        
+	}
+
+	// Update is called once per frame
+	public void openStartGameOptions() 
+	{
+        //Debug.Log("Start Game has been clicked");
+        
+        if (RP_StartGame.GetBool("isVisible") == true)
+        {
+            RP_StartGame.SetBool("isVisible", false);
+        }
+        else //RP_StartGame.SetBool("isVisible", false);
+        {
+            RP_StartGame.SetBool("isVisible", true);
+        }
+
+	}
+}
