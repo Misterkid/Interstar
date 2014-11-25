0dafcf3c952898bf832733296e98f60d010e6ba5
Mark de Heij <markdeheij@hotmail.com>
2014-11-25 12:56:46 +0100

    Main Menu fixxed, made a new build
    Still need to work on it but it's almost there..

--- Defend And Blend/Assets/Scripts/ProtoGUI/ProtoGUI.cs
+++ Defend And Blend/Assets/Scripts/ProtoGUI/ProtoGUI.cs
@@ -4,15 +4,19 @@ using UnityEngine.UI;
 public class ProtoGUI : MonoBehaviour 
 {
     public Text scoreText;
-	// Use this for initialization
+
+   	// Use this for initialization
 	void Start () 
     {
-	
+        
 	}
 	
 	// Update is called once per frame
-	void Update () 
+    void Update()
     {
-        scoreText.text = "Score: " + GameValues.SCORE;
-	}
+        // Temporary disabled, to avoid the error spam... Hate it! haha
+        
+        //scoreText.text = "Score: " + GameValues.SCORE;
+
+    }
 }
--- Defend And Blend/Assets/UIManagerScript.cs
+++ Defend And Blend/Assets/UIManagerScript.cs
@@ -6,41 +6,102 @@ public class UIManagerScript : MonoBehaviour
   
     public Animator CameraAnimator;
     public Animator BookAnimator;
-    public Animator rp_StartGame;
-    public GameObject wooot;
 
+    public GameObject rp_StartGame;
+    public GameObject rp_Options;
+    public GameObject rp_Highscores;
+    public GameObject rp_Credits;
+    
     void Awake()
     {
-        
+        turnOffAllRightPages();
     }
 
 	// Use this for initialization
 	void Start () 
     {
-       CameraAnimator.SetBool("GameIsPaused", false);
-
-       //rp_StartGame.SetBool("isInVisible", true);
-       //Debug.Log(rp_StartGame.GetBool("isInVisible"));
-
-       //
+       CameraAnimator.SetBool("GameIsPaused", false);     
 	}
 
-    public void Update()
+    public void StartNewGame()
     {
-       
+        Application.LoadLevel("eddy_test");
     }
-	
-	public void StartGame()
+
+	public void openStartGame()
     {
-        if (rp_StartGame.GetBool("isInVisible") == true)
+        //First we need to turn off the other pages.
+        turnOffAllRightPages();
+
+        if (rp_StartGame.active == true)
         {
-            rp_StartGame.SetBool("isInVisible", false);
+            rp_StartGame.SetActive(false);
         }
-        else if (rp_StartGame.GetBool("isInVisible") == false)
+        else if (rp_StartGame.active == false)
         {
-            rp_StartGame.SetBool("isInVisible", true);
+            rp_StartGame.SetActive(true);
+        }
+    }
+    public void openOptions()
+    {
+        //First we need to turn off the other pages.
+        turnOffAllRightPages();
+
+        if (rp_Options.active == true)
+        {
+            rp_Options.SetActive(false);
+        }
+        else if (rp_Options.active == false)
+        {
+            rp_Options.SetActive(true);
         }
     }
 
-   
+    public void openHighscores()
+    {
+        //First we need to turn off the other pages.
+        turnOffAllRightPages();
+
+        if (rp_Highscores.active == true)
+        {
+            rp_Highscores.SetActive(false);
+        }
+        else if (rp_Highscores.active == false)
+        {
+            rp_Highscores.SetActive(true);
+        }
+    }
+
+    public void openCredits()
+    {
+        //First we need to turn off the other pages.
+        turnOffAllRightPages();
+
+        if (rp_Credits.active == true)
+        {
+            rp_Credits.SetActive(false);
+        }
+        else if (rp_Credits.active == false)
+        {
+            rp_Credits.SetActive(true);
+        }
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void turnOffAllRightPages()
+    {
+        rp_StartGame.SetActive(false);
+        rp_Options.SetActive(false);
+        rp_Highscores.SetActive(false);
+        rp_Credits.SetActive(false);
+    }
+
+    void Update()
+    {
+        BookAnimator.Play(0);
+    }
 }
