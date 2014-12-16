7f70fe9e582ca8b013215e04c102231af1b40ba2
Eduard Meivogel <eddymeivogel@gmail.com>
2014-12-16 15:08:41 +0100

    Added Animations, new Umbrella, Fixed some bugs in the monster prefab,
    Added Animations, new Umbrella, Fixed some bugs in the monster prefab

--- Defend And Blend/Assets/Scripts/BlenderCatch.cs
+++ Defend And Blend/Assets/Scripts/BlenderCatch.cs
@@ -9,6 +9,7 @@ public class BlenderCatch : MonoBehaviour
     private Animator cameraAnimator;
     public Animator drawerAnimator;
     public Animator smoothAnimator;
+    public Animator spiceJarsAnimator;
 
     public  GameObject SmoothObject;
     public bool isBlending = false;
@@ -27,53 +28,32 @@ public class BlenderCatch : MonoBehaviour
 	void Update () 
     {
         //Debug.Log(SmoothObject.active);
-        
         //Debug.Log("isBlending" + isBlending);
         if (GameValues.ISPAUSED)
             return;
         
-	    if (Input.GetButtonUp ("Fire1") ) 
+	    if (Input.GetButtonUp ("Fire1") && !isBlending) 
         {
-            isBlending = true;
-
-            if(SmoothObject.active == false)
-            {
-                SmoothObject.active = true;
-                smoothAnimator.SetTrigger("Smooth");
-            }
             for(int i = 0; i < monsters.Count; i++)
             {
 
                 Monster monster = monsters[i].GetComponent<Monster>();
-                //points
-                /*
-                if(monster is Strawberry)
-                {
-                    //for(int m = 1; m < )
-                }
-                 * */
-
-                //Debug.Log(monster);
-
-                
                 monster.Die();
                 GameValues.SCORE++;
-                shakeTime = Time.time +( CamShakeTime + 1000);
             }
             //Debug.Log(smoothAnimator.IsInTransition(0));
             // Made an function when its blending. 
             // isBlending = true;
             Blend();
+            shakeTime = Time.time + CamShakeTime;
+            isBlending = true;
         }
         if (Time.time >= shakeTime && isBlending)
         {
-            cameraAnimator.SetTrigger("StopShake");
+            //Debug.Log("Hello");
+            StopBlend();
+            //cameraAnimator.SetTrigger("StopShake");
             isBlending = false;
-        }
-        //SmoothObject.SetActive(false);
-        if (isBlending == false)
-        {
-            
         }
 
 	}
@@ -95,21 +75,29 @@ public class BlenderCatch : MonoBehaviour
         }
     }
 
-    public void Blend()
+    private void Blend()
     {
         //SmoothObject.active = true;
-        smoothAnimator.SetTrigger("Smooth");
+        if (SmoothObject.activeSelf == false)
+        {
+            //SmoothObject.active = true;
+            SmoothObject.SetActive(true);
+            smoothAnimator.SetTrigger("Smooth");
+        }
         cameraAnimator.SetTrigger("Shake");
         drawerAnimator.SetTrigger("Shake");
+        spiceJarsAnimator.SetTrigger("Shake");
         monsters = new List<GameObject>();
     }
-
-
-    public void AfterBlend()
+    private void StopBlend()
     {
-        smoothAnimator.SetTrigger("StopSmooth");
-       // SmoothObject.SetActive(false);
+        if (SmoothObject.activeSelf == true)
+        {
+            SmoothObject.SetActive(false);
+            //SmoothObject.active = false;
+            smoothAnimator.SetTrigger("StopSmooth");
+        }
+        cameraAnimator.SetTrigger("StopShake");
+        spiceJarsAnimator.SetTrigger("StopShake");
     }
 }
-
-    
