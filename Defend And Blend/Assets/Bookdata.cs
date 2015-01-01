4a5d389bc5cfe58ecd49ef9e598fa49d8b29c92b
mrmaarek <markdeheij@hotmail.com>
2015-01-01 20:00:27 +0100

    Changed a few things with the book.

---
+++ Defend And Blend/Assets/Bookdata.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class Bookdata : MonoBehaviour {
+
+    public InputField UserIDField;
+    public string UserID;
+    public string SessionID;
+
+    public bool hasPressedPlay;
+    
+
+    // Use this for initialization
+	void Start() 
+    {
+        hasPressedPlay = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+         
+	}
+}
